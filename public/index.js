e28b9d3d8aec507e3377b81bc685bd5288f6d6cc
Gethen <auricularimpalement@gmail.com>
2024-09-09 12:24:03 -0600

    Contact form

--- public/index.js
+++ public/index.js
@@ -1,6 +1,7 @@
 var noSelection = '-';
 var impaleOnOff = 'off';
 
+/*
 const searchParams = new URLSearchParams(window.location.search);
 
 if (searchParams.has('selection')) {
@@ -10,6 +11,8 @@ if (searchParams.has('selection')) {
     
     $("#topicInformation").load(mapURL);
 }
+
+*/
 
 /* Functionality for menu
 
@@ -129,3 +132,37 @@ $(".mapArea").on ("click", function(){
     $("#topicInformation").load(mapURL);
 });
 */
+
+// Newsletter Sign-Up Form
+
+function sendMail() {
+    var formParams = {
+        name: document.getElementById('name').value,
+        email: document.getElementById('inputEmail').value,
+    };
+
+    // Validate form input
+    if (!formParams.name || !formParams.email) {
+        alert("Please fill in both the name and email fields.");
+        return;
+    }
+
+    console.log("Name:", formParams.name);
+    console.log("Email:", formParams.email);
+
+    const serviceID = "service_50b37r6";
+    const templateID = "template_lmz58in";
+
+    emailjs.send(serviceID, templateID, formParams)
+        .then((response) => {
+            // Clear form input
+            document.getElementById('name').value = "";
+            document.getElementById('inputEmail').value = "";
+            console.log("Response status:", response.status, response.text);
+            alert("You have successfully signed up for the newsletter.");
+        })
+        .catch((error) => {
+            console.error("Error sending email:", error);
+            alert("There was an error sending your message. Please try again.");
+        });
+}
